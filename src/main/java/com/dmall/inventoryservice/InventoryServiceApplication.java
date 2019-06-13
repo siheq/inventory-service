4cd252c7669d0980dffb822f85eefa00fb3f5fde
Simon He <simon.he@cargosmart.com>
2019-06-13 15:09:43 +0800

    add eureka

--- src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
@@ -2,10 +2,12 @@ package com.dmall.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
